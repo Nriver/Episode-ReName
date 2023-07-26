e0861d0a7ef7d055ae4ef664b5b952ef088ddff6
Nriver <6752679+Nriver@users.noreply.github.com>
2023-07-26 14:45:24 +0800

    fix typo

--- EpisodeReName.py
+++ EpisodeReName.py
@@ -300,13 +300,15 @@ def format_path(file_path):
             return file_path.replace('//', '/').replace('/', '\\')
         return file_path.replace('\\', '/').replace('//', '/')
 
+
 def get_absolute_path(file_path):
     # 获取绝对路径
     if file_path.startswith(r'\\'):
         # samba 路径特殊处理
         return file_path.replace('\\', '/')
     else:
-        return os.path.abspath(target_path.replace('\\', '/'))
+        return os.path.abspath(file_path.replace('\\', '/'))
+
 
 def get_season_cascaded(full_path):
     # 逐级向上解析目录季数
